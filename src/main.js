ca5e7d940e3587bbe8a014741a7cbf26391c0fb2
Khiem Khanh <khiemkhanh.szt@gmail.com>
2026-04-03 15:27:09 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,154 @@
+import "./style.scss";
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+
+const canvas = document.querySelector("#experience-canvas");
+const sizes = { height: window.innerHeight, width: window.innerWidth };
+
+// Loaders
+const textureLoader = new THREE.TextureLoader();
+
+// Model Loader
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath("/draco/");
+
+const loader = new GLTFLoader();
+loader.setDRACOLoader(dracoLoader);
+
+// Cube Texture Loader
+const environmentMap = new THREE.CubeTextureLoader()
+  .setPath("textures/skybox/")
+  .load(["px.webp", "nx.webp", "py.webp", "ny.webp", "pz.webp", "nz.webp"]);
+
+const textureMap = {
+  One: { day: "/textures/room/day/TS1.webp" },
+  Two: { day: "/textures/room/day/TS2-4.webp" },
+  Three: { day: "/textures/room/day/TS3.webp" },
+  Four: { day: "/textures/room/day/TS4.webp" },
+  Five: { day: "/textures/room/day/TS5-1.webp" },
+  Six: { day: "/textures/room/day/TS6.webp" },
+  Seven: { day: "/textures/room/day/TS7.webp" },
+};
+
+const loadedTexture = {
+  day: {},
+};
+
+Object.entries(textureMap).forEach(([key, paths]) => {
+  const dayTexture = textureLoader.load(paths.day);
+  dayTexture.flipY = false;
+  dayTexture.colorSpace = THREE.SRGBColorSpace;
+  loadedTexture.day[key] = dayTexture;
+});
+
+const glassMaterial = new THREE.MeshPhysicalMaterial({
+  transmission: 1,
+  opacity: 1,
+  metalness: 0,
+  roughness: 0,
+  ior: 1.5,
+  thickness: 0.01,
+  specularIntensity: 1,
+  envMapIntensity: 1,
+  envMap: environmentMap,
+});
+
+const videoElement = document.createElement("video");
+videoElement.src = "textures/video/Screen.mp4";
+videoElement.loop = true;
+videoElement.muted = true;
+videoElement.playsInline = true;
+videoElement.autoplay = true;
+videoElement.play();
+
+const videoTexture = new THREE.VideoTexture(videoElement);
+videoTexture.colorSpace = THREE.SRGBColorSpace;
+videoTexture.flipY = false;
+
+loader.load("/models/Room_Portfolio_V1.glb", (glb) => {
+  glb.scene.traverse((child) => {
+    if (child.isMesh) {
+      if (child.name.includes("Glass")) {
+        child.material = glassMaterial;
+      } else if (child.name.includes("Screen")) {
+        child.material = new THREE.MeshBasicMaterial({
+          map: videoTexture,
+        });
+      } else {
+        Object.keys(textureMap).forEach((key) => {
+          if (child.name.includes(key)) {
+            const material = new THREE.MeshBasicMaterial({
+              map: loadedTexture.day[key],
+            });
+
+            child.material = material;
+
+            if (child.material.map) {
+              child.material.map.minFilter = THREE.LinearFilter;
+            }
+          }
+        });
+      }
+    }
+  });
+  scene.add(glb.scene);
+});
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  1000
+);
+
+camera.position.set(15.013087608044822, 8.219567956753812, 12.040122376671192);
+
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.update();
+controls.target.set(
+  0.12955454558590865,
+  1.1045767981948078,
+  -0.7946193309291271
+);
+
+// Event Listeners
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Upadate camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update Renderer
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+const render = (time) => {
+  controls.update();
+
+  //   console.log(camera.position);
+  //   console.log("------");
+  //   console.log(controls.target);
+
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(render);
+};
+
+render();
