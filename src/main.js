ff4947c46845c4be94573e678f36f490e4ca5a6a
Khiem Khanh <khiemkhanh.szt@gmail.com>
2026-04-06 13:46:23 +0700

    starting modals working basic

--- src/main.js
+++ src/main.js
@@ -3,11 +3,58 @@ import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import gsap from "gsap";
 
 const canvas = document.querySelector("#experience-canvas");
 const sizes = { height: window.innerHeight, width: window.innerWidth };
 
+const modals = {
+  work: document.querySelector(".modal.work"),
+  about: document.querySelector(".modal.about"),
+  contact: document.querySelector(".modal.contact"),
+};
+
+document.querySelectorAll(".modal-exit-button").forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const modal = e.target.closest(".modal");
+    hideModal(modal);
+  });
+});
+
+const showModal = (modal) => {
+  modal.style.display = "block";
+
+  gsap.set(modal, { opacity: 0 });
+
+  gsap.to(modal, {
+    opacity: 1,
+    duration: 0.5,
+  });
+};
+
+const hideModal = (modal) => {
+  gsap.to(modal, {
+    opacity: 0,
+    duration: 0.5,
+    onComplete: () => {
+      modal.style.display = "none";
+    },
+  });
+};
+
 const zAxixFans = [];
+
+const raycasterObjects = [];
+let currentIntersects = [];
+
+const socialLinks = {
+  Github: "https://github.com",
+  LinkedIn: "https://linkedin.com",
+  Screen: "https://health.khiemszeto.com/vitals",
+};
+
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
 
 // Loaders
 const textureLoader = new THREE.TextureLoader();
@@ -69,9 +116,41 @@ const videoTexture = new THREE.VideoTexture(videoElement);
 videoTexture.colorSpace = THREE.SRGBColorSpace;
 videoTexture.flipY = false;
 
-loader.load("/models/Room_Portfolio_V4.glb", (glb) => {
+window.addEventListener("mousemove", (e) => {
+  pointer.x = (e.clientX / sizes.width) * 2 - 1;
+  pointer.y = -(e.clientY / sizes.height) * 2 + 1;
+});
+
+window.addEventListener("click", (e) => {
+  if (currentIntersects.length > 0) {
+    const object = currentIntersects[0].object;
+
+    Object.entries(socialLinks).forEach(([key, url]) => {
+      if (object.name.includes(key)) {
+        const newWindow = window.open();
+        newWindow.opener = null;
+        newWindow.location = url;
+        newWindow.target = "_blank";
+        newWindow.rel = "noopener noreferrer";
+      }
+    });
+
+    if (object.name.includes("Work_Button")) {
+      showModal(modals.work);
+    } else if (object.name.includes("Contact_Button")) {
+      showModal(modals.contact);
+    } else if (object.name.includes("About_Button")) {
+      showModal(modals.about);
+    }
+  }
+});
+
+loader.load("/models/Room_Portfolio_V5.glb", (glb) => {
   glb.scene.traverse((child) => {
     if (child.isMesh) {
+      if (child.name.includes("Raycaster")) {
+        raycasterObjects.push(child);
+      }
       if (child.name.includes("Glass")) {
         child.material = glassMaterial;
       } else if (child.name.includes("Screen")) {
@@ -156,6 +235,27 @@ const render = (time) => {
   zAxixFans.forEach((fan) => {
     fan.rotation.z -= 0.06;
   });
+
+  // Raycaster
+  raycaster.setFromCamera(pointer, camera);
+
+  currentIntersects = raycaster.intersectObjects(raycasterObjects);
+
+  for (let i = 0; i < currentIntersects.length; i++) {
+    // currentIntersects[i].object.material.color.set(0xff0000);
+  }
+
+  if (currentIntersects.length > 0) {
+    const currentIntersectsObject = currentIntersects[0].object;
+
+    if (currentIntersectsObject.name.includes("Pointer")) {
+      document.body.style.cursor = "pointer";
+    } else {
+      document.body.style.cursor = "default";
+    }
+  } else {
+    document.body.style.cursor = "default";
+  }
 
   renderer.render(scene, camera);
 
